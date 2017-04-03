dbdaedc2d56284b2acf3e7b93275867cc073a8c5
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-03 18:42:10 +0300

    Add slug attribute to WorkflowNew table

---
+++ src/ggrc_workflows/migrations/versions/20170403120005_9a69ccb4035_add_workflow_new_model_table.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2017 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+"""
+Add table for WorkflowNew model
+
+Create Date: 2017-04-03 12:00:05.686115
+"""
+# disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=invalid-name
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9a69ccb4035'
+down_revision = '1142135ce819'
+
+table_name = 'workflows_new'
+
+
+def upgrade():
+  """Upgrade database schema and/or data, creating a new revision."""
+  op.create_table(
+    table_name,
+    sa.Column('slug', sa.String(length=250), nullable=False),
+  )
+  op.create_unique_constraint('uq_{}'.format(table_name), table_name, ["slug"])
+
+
+def downgrade():
+  """Downgrade database schema and/or data back to the previous revision."""
+  op.drop_table(table_name)
