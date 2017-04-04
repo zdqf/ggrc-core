11758365493002e75a60a9687237b0e1c5f29a9f
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-04 15:16:36 +0300

    Add 'status' attribute to WorkflowNew model

--- src/ggrc_workflows/models/workflow_new.py
+++ src/ggrc_workflows/models/workflow_new.py
@@ -1,6 +1,9 @@
 from ggrc import db
 from ggrc.models.mixins import Slugged
+from ggrc.models.mixins import Stateful
 
 
-class WorkflowNew(db.Model, Slugged):
+class WorkflowNew(Stateful, Slugged, db.Model):
   __tablename__ = 'workflows_new'
+
+  VALID_STATES = (u"Not Started", u"In Progress", u"Completed")
