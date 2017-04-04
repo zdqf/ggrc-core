8e78c7f999557383b003f72f4bb61f651f55d379
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-04 16:28:02 +0300

    Add 'description' attribute to WorkflowNew model

--- src/ggrc_workflows/models/workflow_new.py
+++ src/ggrc_workflows/models/workflow_new.py
@@ -1,9 +1,10 @@
 from ggrc import db
 from ggrc.models.mixins import Slugged
 from ggrc.models.mixins import Stateful
+from ggrc.models.mixins import Described
 
 
-class WorkflowNew(Stateful, Slugged, db.Model):
+class WorkflowNew(Described, Stateful, Slugged, db.Model):
   __tablename__ = 'workflows_new'
 
   VALID_STATES = (u"Not Started", u"In Progress", u"Completed")
