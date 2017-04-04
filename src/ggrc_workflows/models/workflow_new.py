4901b1b8e3f31c1f7868191040169b5d26dbc6e1
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-04 17:11:41 +0300

    Add 'title' attribute to WorkflowNew model

--- src/ggrc_workflows/models/workflow_new.py
+++ src/ggrc_workflows/models/workflow_new.py
@@ -2,9 +2,11 @@ from ggrc import db
 from ggrc.models.mixins import Slugged
 from ggrc.models.mixins import Stateful
 from ggrc.models.mixins import Described
+from ggrc.models.mixins import Titled
 
 
-class WorkflowNew(Described, Stateful, Slugged, db.Model):
+class WorkflowNew(Described, Stateful, Slugged, Titled, db.Model):
   __tablename__ = 'workflows_new'
+  _title_uniqueness = False
 
   VALID_STATES = (u"Not Started", u"In Progress", u"Completed")
