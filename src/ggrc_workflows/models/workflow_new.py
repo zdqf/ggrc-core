f5dcf9d248b9224b0104bc0216543f91062b7dc6
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-04 20:03:06 +0300

    Add 'repeat_every' attribute to WorkflowNew model

--- src/ggrc_workflows/models/workflow_new.py
+++ src/ggrc_workflows/models/workflow_new.py
@@ -1,4 +1,5 @@
 from ggrc import db
+from ggrc.models.deferred import deferred
 from ggrc.models.mixins import Slugged
 from ggrc.models.mixins import Stateful
 from ggrc.models.mixins import Described
@@ -10,3 +11,5 @@ class WorkflowNew(Described, Stateful, Slugged, Titled, db.Model):
   _title_uniqueness = False
 
   VALID_STATES = (u"Not Started", u"In Progress", u"Completed")
+
+  repeat_every = deferred(db.Column(db.Integer), 'WorkflowNew')
