ab535c96fe36f19bb1458428d365ffd7227b3413
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-05 16:40:10 +0300

    Add slug attribute to Task model

--- src/ggrc_workflows/models/task.py
+++ src/ggrc_workflows/models/task.py
@@ -4,10 +4,11 @@
 """Module contains 'Task' model implementation."""
 from ggrc import db
 from ggrc.models.mixins import Described
+from ggrc.models.mixins import Slugged
 from ggrc.models.mixins import Titled
 
 
-class Task(Described, Titled, db.Model):
+class Task(Described, Slugged, Titled, db.Model):
   """Contains 'Task' model implementation."""
   __tablename__ = 'tasks'
   _title_uniqueness = False
