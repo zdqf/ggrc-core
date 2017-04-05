63f8a42e840cac129379730694fcbb3b4dd85ea1
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-05 16:24:25 +0300

    Add migration script for Task model

---
+++ src/ggrc_workflows/migrations/versions/20170405132322_3e5f76422a7b_add_task_model.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2017 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""
+Add task model.
+
+Create Date: 2017-04-05 13:23:22.022321
+"""
+# disable Invalid constant name pylint warning for mandatory Alembic variables.
+# pylint: disable=invalid-name
+
+import sqlalchemy as sa
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '3e5f76422a7b'
+down_revision = '9a69ccb4035'
+
+
+def upgrade():
+  """Upgrade database schema and/or data, creating a new revision."""
+
+
+def downgrade():
+  """Downgrade database schema and/or data back to the previous revision."""
+
