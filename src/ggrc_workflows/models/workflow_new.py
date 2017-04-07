b1282caf23194d672e8580e148450907894401cd
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-07 21:41:25 +0300

    Add status property to WorlflowNew model

--- src/ggrc_workflows/models/workflow_new.py
+++ src/ggrc_workflows/models/workflow_new.py
@@ -4,12 +4,14 @@
 """Module contains new 'Workflow' model implementation."""
 from sqlalchemy.orm import validates
 from sqlalchemy.sql import exists
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from ggrc import db
 from ggrc.models.deferred import deferred
 from ggrc.models.mixins import Slugged
 from ggrc.models.mixins import Described
 from ggrc.models.mixins import Titled
+from ggrc_workflows.models.task import Task
 
 
 class WorkflowNew(Described, Slugged, Titled, db.Model):
@@ -21,6 +23,10 @@ class WorkflowNew(Described, Slugged, Titled, db.Model):
   MONTH_UNIT = u'month'
   VALID_UNITS = (DAY_UNIT, MONTH_UNIT)
 
+  NOT_STARTED_STATUS = u'Not Started'
+  IN_PROGRESS_STATUS = u'In Progress'
+  COMPLETED_STATUS = u'Completed'
+
   repeat_every = deferred(db.Column(db.Integer), 'WorkflowNew')
   unit = deferred(db.Column(db.Enum(*VALID_UNITS)), 'WorkflowNew')
   parent_id = deferred(
@@ -30,6 +36,35 @@ class WorkflowNew(Described, Slugged, Titled, db.Model):
   children = db.relationship('WorkflowNew')
   parent = db.relationship('WorkflowNew', remote_side='WorkflowNew.id')
   tasks = db.relationship('Task', back_populates='workflow')
+
+  @hybrid_property
+  def is_recurrent(self):
+    """Calculates property which shows is workflow recurrent or not."""
+    # cycle couldn't be recurrent or not
+    if self.parent:
+      return None
+    return self.repeat_every is not None
+
+  @hybrid_property
+  def status(self):
+    """Calculates status of the workflow."""
+    # cycle is a workflow which has parent workflow
+    # cycle's status always equals None
+    if self.parent:
+      return None
+    if not self.tasks:
+      return self.NOT_STARTED_STATUS
+    if self.is_recurrent:
+      return self.IN_PROGRESS_STATUS
+
+    not_finished_cycle_tasks = db.session.query(Task).filter(
+        Task.workflow_id == WorkflowNew.id,
+        WorkflowNew.parent_id == self.id,
+        Task.status != Task.FINISHED_STATUS
+    )
+    if db.session.query(not_finished_cycle_tasks.exists()).scalar():
+      return self.IN_PROGRESS_STATUS
+    return self.COMPLETED_STATUS
 
   @validates('unit')
   def validate_unit(self, _, value):
