93e45ff89f16e31abb9099b96167a81ed592dd52
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-10 20:36:06 +0300

    Cover Task model's unittests with ddt module

--- test/unit/ggrc_workflows/models/test_task.py
+++ test/unit/ggrc_workflows/models/test_task.py
@@ -3,36 +3,71 @@
 
 """Module contains unittests for Task model."""
 import unittest
+from ddt import data, ddt, unpack
+from mock import patch, MagicMock, PropertyMock
 
-from ggrc_workflows.models.task import Task
+from ggrc_workflows.models import task
 
 
+NOT_STARTED_STATUS = u'Not Started'
+IN_PROGRESS_STATUS = u'In Progress'
+FINISHED_STATUS = u'Finished'
+TEMPLATE_STATUS = u'Template'
+BAD_STATUS = u'bad_status'
+
+INVAL_STAT_MSG = u"Task invalid status: '{}'"
+TEMPL_STAT_MSG = u"Task template must have '{}' status"
+NON_TEMPL_STAT_MSG = (u"Non-template task must have one of the statuses: "
+                      u"'{}'")
+
+
+def task_factory(status, ret_is_templ, is_templ):
+  """Create Task() object for Task().validate_status() testing.
+
+  Args:
+      status: Task's status to set up
+      ret_is_templ: Return value for Task().is_template property.
+      is_templ: Mocked Task().is_template property.
+
+  Returns:
+      Task() object with set values and mocked 'is_template' property.
+  """
+  # Note that when Task().status attribute value is assigned,
+  # Task().validate_status() method runs automatically.
+  is_templ.return_value = ret_is_templ
+  test_task = task.Task()
+  test_task.status = status
+  return test_task
+
+
+@ddt
 class TestTask(unittest.TestCase):
   """Class contains unittests for Task model."""
   def setUp(self):
-    self.not_started_status = u'Not Started'
-    self.in_progress_status = u'In Progress'
-    self.finished_status = u'Finished'
-    self.bad_status = u'bad_status'
+    task.hybrid_property = MagicMock()
 
-  def test_validate_status_ok(self):
+  @patch.object(task.Task, 'is_template', new_callable=PropertyMock)
+  @unpack
+  @data((NOT_STARTED_STATUS, False), (IN_PROGRESS_STATUS, False),
+        (FINISHED_STATUS, False), (TEMPLATE_STATUS, True))
+  def test_validate_status_positive(self, status, ret_is_templ, is_templ):
     """Tests positive cases for Task().validate_status() method."""
-    # Note that when Task().status attribute value is assigned,
-    # Task().validate_status() method runs automatically.
-    valid_statuses = (None, self.not_started_status,
-                      self.in_progress_status, self.finished_status)
-    for status in valid_statuses:
-      task = Task()
-      task.status = status
-      self.assertEqual(task.status, status)
+    self.assertEqual(
+        task_factory(status, ret_is_templ, is_templ).status, status)
 
-  def test_validate_status_raises(self):
-    """Tests negative case for Task().validate_status() method."""
-    # Note that when Task().status attribute value is assigned,
-    # Task().validate_status() method runs automatically.
-    task_bad = Task()
+  @patch.object(task.Task, 'is_template', new_callable=PropertyMock)
+  @unpack
+  @data(
+      (BAD_STATUS, True, INVAL_STAT_MSG.format(BAD_STATUS)),
+      (BAD_STATUS, False, INVAL_STAT_MSG.format(BAD_STATUS)),
+      (NOT_STARTED_STATUS, True, TEMPL_STAT_MSG.format(NOT_STARTED_STATUS)),
+      (IN_PROGRESS_STATUS, True, TEMPL_STAT_MSG.format(IN_PROGRESS_STATUS)),
+      (FINISHED_STATUS, True, TEMPL_STAT_MSG.format(FINISHED_STATUS)),
+      (TEMPLATE_STATUS, False, NON_TEMPL_STAT_MSG.format(TEMPLATE_STATUS))
+  )
+  def test_validate_status_raises(self, status, ret_is_templ, err_msg,
+                                  is_templ):
+    """Tests negative cases for Task().validate_status() method."""
     with self.assertRaises(ValueError) as err:
-      task_bad.status = self.bad_status
-    self.assertIsNone(task_bad.status)
-    self.assertEqual(err.exception.message,
-                     u"Task invalid status: '{}'".format(self.bad_status))
+      self.assertIsNone(task_factory(status, ret_is_templ, is_templ).status)
+      self.assertEqual(err.exception.message, err_msg)
