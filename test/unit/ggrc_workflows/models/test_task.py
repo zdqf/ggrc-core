0859d23a58e1b2829f843e8d35077f4398c9f61d
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-11 00:34:34 +0300

    Add unittest for Task is_template attribute

--- test/unit/ggrc_workflows/models/test_task.py
+++ test/unit/ggrc_workflows/models/test_task.py
@@ -21,23 +21,24 @@ NON_TEMPL_STAT_MSG = (u"Non-template task must have one of the statuses: "
                       u"'{}'")
 
 
-def task_factory(status, ret_is_templ, is_templ):
+def task_factory(status, ret_is_templ):
   """Create Task() object for Task().validate_status() testing.
 
   Args:
       status: Task's status to set up
       ret_is_templ: Return value for Task().is_template property.
-      is_templ: Mocked Task().is_template property.
 
   Returns:
       Task() object with set values and mocked 'is_template' property.
   """
   # Note that when Task().status attribute value is assigned,
   # Task().validate_status() method runs automatically.
-  is_templ.return_value = ret_is_templ
-  test_task = task.Task()
-  test_task.status = status
-  return test_task
+  with patch('ggrc_workflows.models.task.Task.is_template',
+             new_callable=PropertyMock) as is_template:
+    is_template.return_value = ret_is_templ
+    test_task = task.Task()
+    test_task.status = status
+    return test_task
 
 
 @ddt
@@ -46,16 +47,13 @@ class TestTask(unittest.TestCase):
   def setUp(self):
     task.hybrid_property = MagicMock()
 
-  @patch.object(task.Task, 'is_template', new_callable=PropertyMock)
   @unpack
   @data((NOT_STARTED_STATUS, False), (IN_PROGRESS_STATUS, False),
         (FINISHED_STATUS, False), (TEMPLATE_STATUS, True))
-  def test_validate_status_positive(self, status, ret_is_templ, is_templ):
+  def test_validate_status_positive(self, status, ret_is_templ):
     """Tests positive cases for Task().validate_status() method."""
-    self.assertEqual(
-        task_factory(status, ret_is_templ, is_templ).status, status)
+    self.assertEqual(task_factory(status, ret_is_templ).status, status)
 
-  @patch.object(task.Task, 'is_template', new_callable=PropertyMock)
   @unpack
   @data(
       (BAD_STATUS, True, INVAL_STAT_MSG.format(BAD_STATUS)),
@@ -65,9 +63,16 @@ class TestTask(unittest.TestCase):
       (FINISHED_STATUS, True, TEMPL_STAT_MSG.format(FINISHED_STATUS)),
       (TEMPLATE_STATUS, False, NON_TEMPL_STAT_MSG.format(TEMPLATE_STATUS))
   )
-  def test_validate_status_raises(self, status, ret_is_templ, err_msg,
-                                  is_templ):
+  def test_validate_status_raises(self, status, ret_is_templ, err_msg):
     """Tests negative cases for Task().validate_status() method."""
     with self.assertRaises(ValueError) as err:
-      self.assertIsNone(task_factory(status, ret_is_templ, is_templ).status)
+      self.assertIsNone(task_factory(status, ret_is_templ).status)
       self.assertEqual(err.exception.message, err_msg)
+
+  @patch('ggrc_workflows.models.task.Task.workflow')
+  def test_is_template(self, wf):
+    """Tests Task().is_template attribute."""
+    type(wf).is_template = PropertyMock(side_effect=(True, False))
+    test_task = task.Task()
+    self.assertEqual(test_task.is_template, True)
+    self.assertEqual(test_task.is_template, False)
