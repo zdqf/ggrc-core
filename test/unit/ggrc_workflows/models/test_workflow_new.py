94ada69555c4788e7d893c7b0f91bc1dd8bc9b73
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-10 23:35:28 +0300

    Refactor WorkflowNew model unittests with ddt

--- test/unit/ggrc_workflows/models/test_workflow_new.py
+++ test/unit/ggrc_workflows/models/test_workflow_new.py
@@ -3,51 +3,51 @@
 
 """Module contains unittests for WorkflowNew model."""
 import unittest
+from ddt import data, ddt
 from mock import patch, MagicMock
 
 from ggrc_workflows.models.workflow_new import WorkflowNew
 
 
+DAY_UNIT = u'Day'
+MONTH_UNIT = u'Month'
+BAD_UNIT = u'Bad Unit'
+
+
+@ddt
 class TestWorkflowNew(unittest.TestCase):
   """Class contains unittests for WorkflowNew model."""
-  def setUp(self):
-    self.day_unit = u'day'
-    self.month_unit = u'month'
-    self.bad_unit = u'bad_unit'
-
-  def test_validate_unit_ok(self):
+  @data(None, DAY_UNIT, MONTH_UNIT)
+  def test_validate_unit_positive(self, unit):
     """Tests positive cases for WorkflowNew().validate_unit() method."""
     # Note that when WorkflowNew().unit attribute value is assigned,
     # WorkflowNew().validate_unit() method runs automatically.
-    valid_units = (None, self.day_unit, self.month_unit)
-    for unit in valid_units:
-      workflow = WorkflowNew()
-      workflow.unit = unit
-      self.assertEqual(workflow.unit, unit)
+    workflow = WorkflowNew()
+    workflow.unit = unit
+    self.assertEqual(workflow.unit, unit)
 
   def test_validate_unit_raises(self):
     """Tests negative case for WorkflowNew().validate_unit() method."""
     # Note that when WorkflowNew().unit attribute value is assigned,
     # WorkflowNew().validate_unit() method runs automatically.
-    workflow_bad = WorkflowNew()
+    workflow = WorkflowNew()
     with self.assertRaises(ValueError) as err:
-      workflow_bad.unit = self.bad_unit
-    self.assertIsNone(workflow_bad.unit)
-    self.assertEqual(err.exception.message,
-                     u"Invalid unit: '{}'".format(self.bad_unit))
+      workflow.unit = BAD_UNIT
+      self.assertIsNone(workflow.unit)
+      self.assertEqual(err.exception.message,
+                       u"Invalid unit: '{}'".format(BAD_UNIT))
 
   @patch('ggrc_workflows.models.workflow_new.exists')
   @patch('ggrc_workflows.models.workflow_new.db.session.query')
-  def test_validate_parent_id_ok(self, query, _):
+  @data(None, 256)
+  def test_validate_parent_id_positive(self, parent_id, query, _):
     """Tests positive cases for WorkflowNew().validate_parent_id() method."""
     # Note that when WorkflowNew().parent_id attribute value is assigned,
     # WorkflowNew().validate_parent_id() method runs automatically.
     query.return_value.scalar = MagicMock(return_value=True)
-    valid_parent_ids = (None, 256)
-    for parent_id in valid_parent_ids:
-      workflow = WorkflowNew()
-      workflow.parent_id = parent_id
-      self.assertEqual(workflow.parent_id, parent_id)
+    workflow = WorkflowNew()
+    workflow.parent_id = parent_id
+    self.assertEqual(workflow.parent_id, parent_id)
 
   @patch('ggrc_workflows.models.workflow_new.exists')
   @patch('ggrc_workflows.models.workflow_new.db.session.query')
@@ -60,7 +60,7 @@ class TestWorkflowNew(unittest.TestCase):
     workflow_bad = WorkflowNew()
     with self.assertRaises(ValueError) as err:
       workflow_bad.parent_id = bad_id
-    self.assertIsNone(workflow_bad.parent_id)
-    self.assertEqual(err.exception.message,
-                     u"Parent workflow with id '{}' is not "
-                     u"found".format(bad_id))
+      self.assertIsNone(workflow_bad.parent_id)
+      self.assertEqual(err.exception.message,
+                       u"Parent workflow with id '{}' is not "
+                       u"found".format(bad_id))
