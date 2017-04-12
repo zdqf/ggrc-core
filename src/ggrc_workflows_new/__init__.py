5d59fc2b9d49395871807437c3d6bfa33886468b
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-12 21:47:56 +0300

    Add blueprint to workflows_new package

--- src/ggrc_workflows_new/__init__.py
+++ src/ggrc_workflows_new/__init__.py
@@ -1,2 +1,7 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+from flask import Blueprint
+
+
+blueprint = Blueprint('ggrc_workflows_new', __name__)
