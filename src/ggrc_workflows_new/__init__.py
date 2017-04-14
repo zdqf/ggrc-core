45131a86bc8fd4a2a5ac59038a63989c7af32b07
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-14 19:44:09 +0300

    Register WorkflowNew service/model

--- src/ggrc_workflows_new/__init__.py
+++ src/ggrc_workflows_new/__init__.py
@@ -2,6 +2,14 @@
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 """Module contains main functions for workflows_new blueprint."""
 import flask
+from ggrc.services import registry
+from ggrc_workflows_new.models import workflow_new
 
 
 blueprint = flask.Blueprint('ggrc_workflows_new', __name__)  # noqa # pylint: disable=invalid-name
+
+
+def contributed_services():
+  return (
+      registry.service('workflows_new', workflow_new.WorkflowNew),
+  )
--- src/ggrc_workflows_new/models/__init__.py
+++ src/ggrc_workflows_new/models/__init__.py
@@ -1,2 +1,8 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+"""Initialize application with ggrc_wokflows_new blueprint's models."""
+from ggrc.models import all_models
+from ggrc_workflows_new.models import workflow_new
+
+
+all_models.register_model(workflow_new.WorkflowNew)
