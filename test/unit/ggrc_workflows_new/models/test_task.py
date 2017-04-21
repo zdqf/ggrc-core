5a33648664944b1bb0367b9e3887462f9cbfb674
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-04-21 12:37:59 +0300

    Add reminder for Task model unittests moving

--- test/unit/ggrc_workflows_new/models/test_task.py
+++ test/unit/ggrc_workflows_new/models/test_task.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 """Module contains unittests for Task model."""
+# TODO: Move validate_status tests to another module
 import unittest
 import ddt
 import mock
