5318e850b3f0406646e5147858d4d48d86fd91e0
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-05-15 15:16:05 +0300

    Fix publish_attrs for TaskComment model

--- src/ggrc_workflows_new/models/task_comment.py
+++ src/ggrc_workflows_new/models/task_comment.py
@@ -9,6 +9,7 @@ from ggrc.models import mixins
 class TaskComment(mixins.Base, mixins.Described, db.Model):
   """Model for Comments on tasks."""
   __tablename__ = 'task_comments'
+  _publish_attrs = ('task',)
   task_id = deferred.deferred(
       db.Column(db.Integer, db.ForeignKey('tasks.id', ondelete='CASCADE'),
                 nullable = False), 'TaskComment')
