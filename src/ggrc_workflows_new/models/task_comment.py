2d37dd9f8476d7b82e487b3d7ea76b2abdbdef12
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-05-15 15:36:09 +0300

    Add validator for TaskComment's body attribute

--- src/ggrc_workflows_new/models/task_comment.py
+++ src/ggrc_workflows_new/models/task_comment.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2017 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 """Module contains 'TaskComment' model implementation."""
+from sqlalchemy import orm
 from ggrc import db
 from ggrc.models import deferred
 from ggrc.models import mixins
@@ -15,3 +16,10 @@ class TaskComment(mixins.Base, db.Model):
       db.Column(db.Integer, db.ForeignKey('tasks.id', ondelete='CASCADE'),
                 nullable = False), 'TaskComment')
   task = db.relationship('Task', back_populates='comments')
+
+  @orm.validates('body')
+  def validate_body(self, _, value):
+    """Make sure that body has valid value during set operation."""
+    if value is None or len(value) == 0:
+      raise ValueError(u"Body shouldn't be empty")
+    return value
