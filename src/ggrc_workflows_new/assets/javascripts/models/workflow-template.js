d690d78d6e377a5fc886d408c3db4ca67868544e
m-vitaly <vitaly851001@gmail.com>
2017-07-10 18:42:27 +0300

    Cleanup WorkflowTemplate model

--- src/ggrc_workflows_new/assets/javascripts/models/workflow-template.js
+++ src/ggrc_workflows_new/assets/javascripts/models/workflow-template.js
@@ -17,23 +17,6 @@
     title_singular: 'Workflow',
     is_custom_attributable: true,
 
-    unit: null,
-    repeat_every: null,
-    ends: null,
-    occurrences: null,
-
-    attributes: {
-      people: 'CMS.Models.Person.stubs',
-      modified_by: 'CMS.Models.Person.stub',
-      context: 'CMS.Models.Context.stub',
-      custom_attribute_values: 'CMS.Models.CustomAttributeValue.stubs',
-      default_lhn_filters: {
-        WorkflowTemplate: {}
-      }
-    },
-    obj_nav_options: {
-      show_all_tabs: true
-    },
     tree_view_options: {
       attr_view: GGRC.mustache_path + '/workflows/tree-item-attr.mustache',
       attr_list: [
@@ -65,72 +48,6 @@
         instance.attr('_redirect', redirectLink);
       });
       return dfd;
-    },
-    // Check if task groups are slated to start
-    //   in the current week/month/quarter/year
-    is_mid_frequency: function () {
-      var dfd = new $.Deferred();
-      var self = this;
-
-      function _afterOrSame(d1, d2) {
-        return d1.isAfter(d2, 'day') || d1.isSame(d2, 'day');
-      }
-      function _beforeOrSame(d1, d2) {
-        return d1.isBefore(d2, 'day') || d1.isSame(d2, 'day');
-      }
-      function checkAllTasks(tasks) {
-        tasks.each(function (task) {
-          var start;
-          var end;
-          var current = moment();
-          task = task.reify();
-          switch (self.frequency) {
-            case 'weekly':
-              start = moment().isoWeekday(task.relative_start_day);
-              end = moment().isoWeekday(task.relative_end_day);
-              if (_afterOrSame(start, end)) {
-                end.add(1, 'w');
-              }
-              break;
-            case 'monthly':
-              start = moment().date(task.relative_start_day);
-              end = moment().date(task.relative_end_day);
-              if (_afterOrSame(start, end)) {
-                end.add(1, 'M');
-              }
-              break;
-            default:
-              dfd.resolve(false);
-          }
-          if (_afterOrSame(current, start) && _beforeOrSame(current, end)) {
-            dfd.resolve(true);
-          }
-        });
-        dfd.resolve(false);
-      }
-
-      if (!this.frequency_duration || this.frequency === 'one_time') {
-        return dfd.resolve(false);
-      }
-
-      // Check each task in the workflow:
-      this.refresh_all('task_group_tasks').then(function (taskGroupTasks) {
-        var tasks = new can.List();
-        taskGroupTasks.each(function (task) {
-          tasks.push(task.reify());
-        });
-        checkAllTasks(tasks);
-      });
-      return dfd;
-    },
-
-    // Get duration from frequency or false for one_time or continuous wfs.
-    frequency_duration: function () {
-      switch (this.frequency) {
-        case 'weekly': return 'week';
-        case 'monthly': return 'month';
-        default: return false;
-      }
     }
   });
 })(window.can, window._);
