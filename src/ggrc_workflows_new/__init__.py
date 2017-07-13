e9d4095747d2964f2e1d3f1d9a4f96ac24443fef
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2017-07-13 15:11:13 +0300

    Cleanup workflow-new models

--- src/ggrc_workflows_new/__init__.py
+++ src/ggrc_workflows_new/__init__.py
@@ -2,25 +2,13 @@
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 """Module contains main functions for ggrc_workflows_new blueprint."""
 # pylint: disable=too-few-public-methods
-from datetime import datetime
 import flask
-import sqlalchemy as sa
 from ggrc import db
-from ggrc import login
 from ggrc.services import registry as registry_service
 from ggrc.views import registry as registry_view
 from ggrc.services import signals
-from ggrc_basic_permissions import contributed_roles
-from ggrc_basic_permissions import models as permission_models
-from ggrc_workflows_new.models import label as label_module
 from ggrc_workflows_new.models import task as task_module
-from ggrc_workflows_new.models import task_comment
 from ggrc_workflows_new.models import workflow_template
-from ggrc_workflows_new.models import workflow_person_new
-from ggrc_workflows_new.roles import BasicWorkflowReaderNew
-from ggrc_workflows_new.roles import WorkflowBasicReaderNew
-from ggrc_workflows_new.roles import WorkflowMemberNew
-from ggrc_workflows_new.roles import WorkflowOwnerNew
 
 
 blueprint = flask.Blueprint('ggrc_workflows_new', __name__)  # noqa # pylint: disable=invalid-name
@@ -31,9 +19,6 @@ def contributed_services():
   return (
       registry_service.service('workflow_templates', workflow_template.WorkflowTemplate),
       registry_service.service('tasks', task_module.Task),
-      registry_service.service('task_comments', task_comment.TaskComment),
-      registry_service.service('workflow_people_new',
-                               workflow_person_new.WorkflowPersonNew),
   )
 
 
@@ -99,29 +84,6 @@ def _update_cycle_task_template(cycle_task):
     db.session.flush()
 
 
-def _setup_cycle_task_label(cycle_task, label_title):
-  for label in cycle_task.workflow.parent.labels:
-    if label.title == label_title:
-      break
-  else:
-    label = label_module.Label(
-        title=label_title,
-        workflow=cycle_task.workflow.parent,
-        context=cycle_task.workflow.parent.context
-    )
-    db.session.add(label)
-  cycle_task.label = label
-  db.session.add(cycle_task)
-  db.session.flush()
-
-
-def _delete_orphan_label(label, exclude_task=None):
-  if not label.tasks or (exclude_task and len(label.tasks) == 1 and
-                         exclude_task in label.tasks):
-    db.session.delete(label)
-    db.session.flush()
-
-
 def _validate_cycle_task_status(cycle_task):
   """Make sure that Task's status value is valid.
 
@@ -141,32 +103,3 @@ def _validate_cycle_task_status(cycle_task):
     raise ValueError(
         u"Non-template task must have one of the statuses: "
         u"'{}'".format(', '.join(cycle_task.NON_TEMPLATE_STATUSES)))
-
-
-def _ensure_assignee_is_workflow_member(cycle_task):  # noqa pylint: disable=invalid-name
-  """Ensure that assignee has role WorkflowMember.
-
-  Args:
-    workflow: Parent WorkflowNew object for task model.
-    assignee: Person object setup for task model.
-  """
-  is_assignee_in_template = False
-  for wp in cycle_task.workflow.parent.workflow_people:
-    if cycle_task.contact == wp.person:
-      is_assignee_in_template = True
-      break
-  if not is_assignee_in_template:
-    workflow_member = workflow_person_new.WorkflowPersonNew(
-        person=cycle_task.contact,
-        workflow=cycle_task.workflow.parent,
-        context=cycle_task.workflow.parent.context
-    )
-    db.session.add(workflow_member)
-    user_role = permission_models.UserRole(
-        person=cycle_task.contact,
-        role=_find_role('WorkflowMemberNew'),
-        context=cycle_task.workflow.parent.context,
-        modified_by=login.get_current_user(),
-    )
-    db.session.add(user_role)
-    db.session.flush()
--- src/ggrc_workflows_new/models/task.py
+++ src/ggrc_workflows_new/models/task.py
@@ -33,14 +33,9 @@ class Task(mixins.Described, mixins.Slugged, mixins.Titled, db.Model):
   # workflow = db.relationship('WorkflowNew', back_populates='tasks')
   status = deferred.deferred(db.Column(
       db.Enum(*VALID_STATUSES), nullable=False), 'Task')
-  label_id = deferred.deferred(
-      db.Column(db.Integer, db.ForeignKey('labels.id')), 'Task')
-  label = db.relationship('Label', back_populates='tasks')
   parent_id = deferred.deferred(
       db.Column(db.Integer,
                 db.ForeignKey('{}.id'.format(__tablename__),
                               ondelete='CASCADE')), 'Task')
   children = db.relationship('Task', cascade='all, delete-orphan')
   parent = db.relationship('Task', remote_side='Task.id')
-  comments = db.relationship('TaskComment', back_populates='task',
-                             cascade='all, delete-orphan')
