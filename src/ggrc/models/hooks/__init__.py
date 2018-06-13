d469bd8fceb31f2ab45244290e5fa24894f10b93
Aliaksei Buziuma <buzuma.leha@gmail.com>
2018-06-13 16:19:35 +0300

    Move import statements inside the function
    This change need for avoid cyclic import of modules in future.

--- src/ggrc/models/hooks/__init__.py
+++ src/ggrc/models/hooks/__init__.py
@@ -3,35 +3,37 @@
 
 """Import GGRC model hooks."""
 
-from ggrc.models.hooks import common
-from ggrc.models.hooks import assessment
-from ggrc.models.hooks import audit
-from ggrc.models.hooks import comment
-from ggrc.models.hooks import custom_attribute_definition
-from ggrc.models.hooks import issue
-from ggrc.models.hooks import issue_tracker
-from ggrc.models.hooks import relationship
-from ggrc.models.hooks import acl
-from ggrc.models.hooks import proposal
-
-
-ALL_HOOKS = [
-    assessment,
-    audit,
-    comment,
-    issue,
-    relationship,
-    custom_attribute_definition,
-    acl,
-    common,
-
-    # Keep IssueTracker at the end of list to make sure that all other hooks
-    # are already executed and all data is final.
-    issue_tracker,
-    proposal,
-]
-
 
 def init_hooks():
-  for hook in ALL_HOOKS:
+  """Initialize all hooks."""
+
+  # Need to import hooks here to avoid cycle import of modules
+  from ggrc.models.hooks import common
+  from ggrc.models.hooks import assessment
+  from ggrc.models.hooks import audit
+  from ggrc.models.hooks import comment
+  from ggrc.models.hooks import custom_attribute_definition
+  from ggrc.models.hooks import issue
+  from ggrc.models.hooks import issue_tracker
+  from ggrc.models.hooks import relationship
+  from ggrc.models.hooks import acl
+  from ggrc.models.hooks import proposal
+
+  all_hooks = [
+      assessment,
+      audit,
+      comment,
+      issue,
+      relationship,
+      custom_attribute_definition,
+      acl,
+      common,
+
+      # Keep IssueTracker at the end of list to make sure that all other hooks
+      # are already executed and all data is final.
+      issue_tracker,
+      proposal,
+  ]
+
+  for hook in all_hooks:
     hook.init_hook()
